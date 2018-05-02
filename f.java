d01e557fe28493dfdd08b1c93669c87376675c07
Admin Sistema <utility.sal001@gmail.com>
2018-05-02 19:38:03 +0200

    Create f.java

---
+++ f.java
@@ -0,0 +1,37 @@
+import java.io.*;
+
+class f
+{
+  public static int leggi_int()
+  {
+    InputStreamReader input = new InputStreamReader(System.in);
+    BufferedReader tastiera = new BufferedReader(input);
+    int x;
+    String help;
+    try
+    {
+      help = tastiera.readLine();
+    }
+    catch(Exception e)
+    {
+      help = "";
+    }
+    x = Integer.valueOf(help).intValue();
+    return x;
+  }
+  public static String leggi_string()
+  {
+    InputStreamReader input = new InputStreamReader(System.in);
+    BufferedReader tastiera = new BufferedReader(input);
+    String help;
+    try
+    {
+      help = tastiera.readLine();
+    }
+    catch(Exception e)
+    {
+      help = "";
+    }
+    return help;
+  }
+}
