05f43cd1de18d2582e14c7fd90d6e9a13f9e1012
Admin Sistema <utility.sal001@gmail.com>
2018-05-03 16:12:18 +0200

    Create area_rettangolo_funzioni.java

---
+++ area_rettangolo_funzioni.java
@@ -0,0 +1,13 @@
+class area_rettangolo_funzioni
+{
+  public static void main(String args[])
+  {
+    int x, y, area;
+    System.out.println("Dimmi il valore della base");
+    x = f.leggi_int();
+    System.out.println("Dimmi il valore dell'altezza");
+    y = f.leggi_int();
+    area = x * y;
+    System.out.println("L'area č "+ area);
+  }
+}
